5d5ed984fa21c10f3c5aee42089c0952b95de58d
Ian Wytia <wytia.ian@gmail.com>
2011-01-25 20:52:23 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redneckrails::Application.config.secret_token = '645833a7c96437bd9e88ec1f8b626b77355b897af8b2a63f0cce80f90f7fa5af8744b30bca3d91f88a291e124f80f1db5e1ab2dcd1c83df2996ff8e21373d857'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Redneckrails::Application.config.session_store :cookie_store, :key => '_redneckrails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Redneckrails::Application.config.session_store :active_record_store
